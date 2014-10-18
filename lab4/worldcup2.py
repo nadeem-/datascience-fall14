0af65f5077d8c881a922f40e61ae8fff8b63ab6a
Nadeem Malik <nadeem244@gmail.com>
2014-10-18 03:24:18 -0400

    wrangler

---
+++ lab4/worldcup2.py
@@ -0,0 +1,49 @@
+import re
+import fileinput
+import csv
+import pandas
+import numpy
+
+file_name = "worldcup.txt"
+import fileinput
+ 
+lines = []
+for line in fileinput.input(file_name, inplace=False):
+	line = re.sub('(\\|)+(\d)+(\\|)+.+', "", line.rstrip())
+	line = re.sub('\\||{|}|fb|style=\".*\"', "", line.rstrip())
+	line = re.sub('\\[|\\]', "", line.rstrip())
+	line = re.sub('FIFA World Cup(\d){4}(#\d\\*)*', "", line.rstrip())
+	line = re.sub('#\d\\^', "", line.rstrip())
+	line = re.sub('(^style=.+)|-|<sup>...</sup>|\\(|\\)', "", line.rstrip())
+	line = re.sub(' , ', " ", line.rstrip())
+	lines.append(line)
+
+
+num_lines = len(lines)
+i = 0
+
+outputFile = open("output.csv", 'wb')
+wr = csv.write(outputFile);
+
+while(not re.match('[A-Z]{3}', lines[i])):
+	i += 1
+
+csv = []
+csv.append(["country", "year", "placing"])
+
+while(i < num_lines and re.match('[A-Z]{3}', lines[i])):
+	country = lines[i].rstrip()
+	x = [1,2,3,4]
+	for placing in x:
+		l2 = lines[i+placing]
+		if(not re.match('align=centersort dash', l2)):
+			for year in l2.split(" "):
+				if len(year) > 1:
+					csv.append([country, year, str(placing)])
+					#csv.append(country + ", " + year + ", " + str(placing))
+	i += 5
+
+	while(i < num_lines and not re.match('[A-Z]{3}', lines[i])):
+		i += 1
+
+wr.writerows(csv)
